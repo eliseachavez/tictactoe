e15d17b4fc1bb5bc6ac34aa721440a471efa2633
lemur <noctiphobic@gmail.com>
2022-06-16 14:25:38 -0500

    Add learned-nuggets file with notes & Abstract the logic of the game into one Game class

--- t.rb
+++ t.rb
@@ -3,102 +3,76 @@
 # where two human players can play against each
 # other and the board is displayed in between turns.
 
-# How this game logic will work:
-# have a hash, with a list for each possible win sequence
-# any time a player makes a move, check for win sequences in the existing sequence.
-$turn = true
-$over = false
-win_sequences = [
-  ["nw-nc-ne"], ["ne-nc-nw"],
-  ["wc-c-ec"], ["ec-c-wc"],
-  ["sw-sc-se"], ["se-sc-sw"],
-  ["nw-c-se"], ["se-c-nw"],
-  ["nc-c-sc"], ["sc-c-nc"],
-  ["nw-wc-sw"], ["sw-wc-nw"],
-  ["ne-ec-se"], ["se-ec-ne"],
-  ["sw-c-ne"], ["ne-c-sw"]
-]
-class Tile
-  def initialize(name)
-    @name = name
+class Game
+  # true for all game instances
+  @@win_sequences = [
+    ["nw-nc-ne"], ["ne-nc-nw"],
+    ["wc-c-ec"], ["ec-c-wc"],
+    ["sw-sc-se"], ["se-sc-sw"],
+    ["nw-c-se"], ["se-c-nw"],
+    ["nc-c-sc"], ["sc-c-nc"],
+    ["nw-wc-sw"], ["sw-wc-nw"],
+    ["ne-ec-se"], ["se-ec-ne"],
+    ["sw-c-ne"], ["ne-c-sw"]
+  ]
+  # individual game specific
+  attr_accessor @over = false
+  @board = [' ', 'x', ' ', ' ', ' ', 'x', 'x', 'o', 'o']
+  @p1_turn = true
+
+  def initialize
+   puts "Welcome to tic tac toe. This is a 2 player game. "
+    until Game.@over do
+      Game.play
+    end
   end
 
   public
 
-  def choose_tile
-  # add a neighbor
-    puts "hi"
+  def play
+    selection = make_selection
+    if @p1_turn
+      @board[selection] = 'x'
+      @p1_turn = false
+    else
+      @board[selection] = 'o'
+      @p1_turn = true # p2 is done, is now p1s turn
+    end
+    # now check to see if there's a hit
+    is_game_over?
   end
-end
 
-def make_board
-  t1 = Tile.new("t1")
-  t2 = Tile.new("t2")
-  t3 = Tile.new("t3")
-  t4 = Tile.new("t4")
-  t5 = Tile.new("t5")
-  t6 = Tile.new("t6")
-  t7 = Tile.new("t7")
-  t8 = Tile.new("t8")
-  t9 =Tile.new("t9")
-end
+  private
 
-def prompt_turn
-  selection = nil
-  if $turn == true
-    turn = !turn
-    puts "Player 1, it is your turn"
-    get_input
-  else
-    turn = !turn
-    puts "Player 2, it is your turn"
-    get_input
+  def make_selection(mark) # mark is a 'x' or 'o'
+    puts "Here is the current board:"
+    print_board
+    puts "Choose from the remaining tiles: 1, 2, 3, 4, 5, 6, 7, 8, 9"
+    begin
+      selection = gets.chomp
+    rescue StandardError=>e
+      puts "Error: #{e}"
+    else
+      puts "You chose #{selection}"
+      selection -= 1 # so that it's zero indexed
+    end
   end
-end
 
-def get_input
-  puts "Here is the current board;"
-  puts "Choose from the remaining tiles."
-  print_board
-  begin
-    selection = gets.chomp
-  rescue StandardError=>e
-    puts "Error: #{e}"
-  else
-    puts "You chose #{selection}"
-    index = $board.index(selection)
-    # now get the tile object that corresponds
-    choose_tile($board[index])
+  def print_board
+    @board.each_index do |index|
+      print "[ "
+      print @board[index]
+      print " ]"
+      if index == 2 || index == 5 || index == 8
+        puts
+      end
+    end
   end
-end
 
-#def print_board
-  #board_rows = [["___"], ["___"], ["___"]]
-  #board_columns = [["|"], ["|"], ["|"]]
-#  puts " ___ ___ ___ "
-#  3.times do
-#    print "|"
-#    3.times do
-#      print "___"
-#      1.times do
-#        print "|"
-#      end
-#    end
-#    puts
-#  end
-#end
-
-def print_board
- board = {
-  t1: t1,
-
- }
-  3 times (so below is individual row)
+  def is_game_over?
+    #compare win sequences to x or o
+  end
 end
 
 # MAIN #############
-make_board
-
-until $over do
-  prompt_turn
-end
+game = Game.new
